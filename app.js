dd4d96d31cbe9e057e33c5de307b9cc8d44544e5
Mohammad Anees <mohammad.irock@gmail.com>
2016-04-01 13:30:48 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// Docs at http://simpleweatherjs.com
+
+/* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+/* Where in the world are you? */
+$('.js-geolocation').on('click', function() {
+  // console.log('click');
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log('click');
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+});
+
+/* 
+* Test Locations
+* Austin lat/long: 30.2676,-97.74298
+* Austin WOEID: 2357536
+*/
+$(document).ready(function() {
+  loadWeather('Seattle',''); //@params location, woeid
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
