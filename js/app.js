8d910ef2e6d5aafa38d81f5f34606622fcefd287
Mohammad Anees <mohammad.irock@gmail.com>
2016-04-05 10:28:42 -0500

    Restructure code, add mockup sketch file

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Docs at http://simpleweatherjs.com
+
+/* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+/* 
+* Test Locations
+* Austin lat/long: 30.2676,-97.74298
+* Austin WOEID: 2357536
+*/
+$(document).ready(function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log('click');
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      console.log(weather.code);
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>'; 
+
+      for(var i = 0; i < 5; ++i) {
+             html += '<p>'+weather.forecast[i].date+': '+weather.forecast[i].high+'</p>';
+           }
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
