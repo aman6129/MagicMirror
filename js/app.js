bc23725c60582b68d1059056267041a41c845655
Mohammad Anees <mohammad.irock@gmail.com>
2016-04-06 01:11:53 -0500

    add dynamic weather and icon

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-var date = new Date();
-
 /* Does your browser support geolocation? */
 if ("geolocation" in navigator) {
   $('.js-geolocation').show(); 
@@ -9,17 +7,23 @@ if ("geolocation" in navigator) {
 
 function formatDate()
 {
+  var date = new Date();
   var hours = date.getHours();
   var min = date.getMinutes();
   var ampm = "AM";
-  if(date.getHours() > 12){
+
+  if(date.getHours > 12){
     hours -= 12;
     ampm = "PM"
   }
   if(min < 10)
-    $('#time').append(hours + ':0' + min + ' ' + ampm);
-  else
-    $('#time').append(hours + ':' + min + ' ' + ampm);
+    min = "0" + min;
+  if(hours < 10)
+    hours = "0" + hours;
+
+  $('#time').html(hours + ':' + min + ' ' + ampm);
+
+  t = setTimeout(formatDate,1000);
 
 }
 
@@ -30,7 +34,7 @@ function formatDate()
 */
 $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    // loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
   });
 
   formatDate();
@@ -42,17 +46,16 @@ function loadWeather(location, woeid) {
     woeid: woeid,
     unit: 'f',
     success: function(weather) {
-      console.log(weather.code);
-      html = '<h2><i class="wi wi-yahoo-'+weather.code+'"></i> '+weather.temp+'&deg;</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>'; 
+      // html = '<h2><i class="wi wi-yahoo-'+weather.code+'"></i> '+weather.temp+'&deg;</h2>';
+      // html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      // html += '<li class="currently">'+weather.currently+'</li>';
+      // html += '<li>'+weather.alt.temp+'&deg;C</li></ul>'; 
 
-      for(var i = 0; i < 5; ++i) {
-             html += '<p>'+weather.forecast[i].date+': '+weather.forecast[i].high+ '/' + weather.forecast[i].low + '</p>';
-           }
-      $('#location').append('<p>' + weather.city + ', ' + weather.region + '</p>');
-      $("#weather").html(html);
+      // for(var i = 0; i < 5; ++i) {
+      //        html += '<p>'+weather.forecast[i].date+': '+weather.forecast[i].high+ '/' + weather.forecast[i].low + '</p>';
+      //      }
+      // $('#location').append('<p>' + weather.city + ', ' + weather.region + '</p>');
+      $(".weather-big").append('<i class="wi wi-yahoo-' + weather.code + '"></i>&nbsp' + weather.temp + '&deg');
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
